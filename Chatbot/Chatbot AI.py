c4af95cbbb2b7fdd6d6ca5fdfca46e6e50054afc
Pankaj <mehrapankaj8885@gmail.com>
2025-09-07 15:34:57 +0530

    Update and rename Chatbot AI.py to Chatbot/Chatbot AI.py

--- Chatbot/Chatbot AI.py
+++ Chatbot/Chatbot AI.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = os.getenv("API_KEY")
 
 genai.configure(api_key=api_key)
 
@@ -19,3 +19,4 @@ if __name__ == "__main__":
             break
         response = ask_gemini(user_input)
         print("Gemini:", response)
+
